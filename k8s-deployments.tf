65656570969406511124ec46333303b61a2da830
DSVB Thribhuvan <thribhuvan.d@mannkey.com>
2025-07-21 11:49:47 +0530

    wip: prod fronted deployment

--- k8s-deployments.tf
+++ k8s-deployments.tf
@@ -105,7 +105,7 @@ resource "kubernetes_deployment" "pharmetrade" {
       spec {
         container {
           name  = "pharmetrade"
-          image = "headway630/pharmetrade:latest"
+          image = "headway630/pharmaetrade-v1:v1"
 
           env {
             name  = "VITE_API_BASE_URL"
